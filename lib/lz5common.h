f9ab21314a578d82daf826785b754563ef8f2094
inikep <inikep@gmail.com>
2016-01-06 20:27:05 +0100

    binary tree

--- lib/lz5common.h
+++ lib/lz5common.h
@@ -289,7 +289,7 @@ typedef struct
 /* *************************************
 *  HC Pre-defined compression levels
 ***************************************/
-#define LZ5HC_MAX_CLEVEL 18
+#define LZ5HC_MAX_CLEVEL 15
 
 static const int g_maxCompressionLevel = LZ5HC_MAX_CLEVEL;
 static const int LZ5HC_compressionLevel_default = 6;
@@ -297,28 +297,22 @@ static const int LZ5HC_compressionLevel_default = 6;
 static const LZ5HC_parameters LZ5HC_defaultParameters[LZ5HC_MAX_CLEVEL+1] =
 {
     /* W,  C,  H, H3,  Snum, SL, SuffL, FS, Strategy */
-    {  0,  0,  0,  0,     0,  0,     0,  0, LZ5HC_fast         },  // level 0 - never used
-    { 22, 22, 13,  0,     4,  6,     0,  0, LZ5HC_fast         },  // level 1
-    { 22, 22, 13,  0,     2,  6,     0,  0, LZ5HC_fast         },  // level 2
-    { 22, 22, 13,  0,     1,  5,     0,  0, LZ5HC_fast         },  // level 3
-    { 22, 22, 14, 13,     1,  4,     0,  0, LZ5HC_price_fast   },  // level 4
-    { 22, 22, 17, 13,     1,  4,     0,  0, LZ5HC_price_fast   },  // level 5
-    { 22, 22, 15, 13,     1,  4,     0,  0, LZ5HC_lowest_price },  // level 6
-    { 22, 22, 17, 13,     1,  4,     0,  0, LZ5HC_lowest_price },  // level 7
-    { 22, 22, 19, 16,     1,  4,     0,  0, LZ5HC_lowest_price },  // level 8
-    { 22, 22, 23, 16,     3,  4,     0,  0, LZ5HC_lowest_price },  // level 9
-    { 22, 22, 23, 16,     8,  4,     0,  0, LZ5HC_lowest_price },  // level 10
-    { 22, 22, 23, 16,     8,  4,    12,  0, LZ5HC_optimal_price }, // level 11
-    { 22, 22, 23, 16,     8,  4,    64,  0, LZ5HC_optimal_price }, // level 12
-    { 22, 22, 23, 16,     8,  4,    64,  1, LZ5HC_optimal_price }, // level 13
-    { 22, 23 , 23, 16,     8,  4,    64,  1, LZ5HC_optimal_price_bt }, // level 14
-    { 22, 22, 23, 16,    32,  4,    64,  0, LZ5HC_optimal_price }, // level 15
-    { 22, 22, 23, 16,   128,  4,    64,  1, LZ5HC_optimal_price }, // level 16
-    { 22, 23 , 23, 16,   128,  4,    64,  1, LZ5HC_optimal_price_bt }, // level 17
-//    { 22, 22, 23, 16,   128,  4,    64,  0, LZ5HC_optimal_price }, // level 15
-//    { 22, 22, 23, 16,   512,  4,    64,  0, LZ5HC_optimal_price }, // level 16
-//    { 22, 22, 23, 16,   512,  4,    64,  1, LZ5HC_optimal_price }, // level 17
-//    { 22, 22, 28, 24, 1<<10,  4, 1<<10,  1, LZ5HC_optimal_price }, // level 18
+    {  0,  0,  0,  0,     0,  0,     0,  0, LZ5HC_fast             },  // level 0 - never used
+    { 22, 22, 13,  0,     4,  6,     0,  0, LZ5HC_fast             },  // level 1
+    { 22, 22, 13,  0,     2,  6,     0,  0, LZ5HC_fast             },  // level 2
+    { 22, 22, 13,  0,     1,  5,     0,  0, LZ5HC_fast             },  // level 3
+    { 22, 22, 14, 13,     1,  4,     0,  0, LZ5HC_price_fast       },  // level 4
+    { 22, 22, 17, 13,     1,  4,     0,  0, LZ5HC_price_fast       },  // level 5
+    { 22, 22, 15, 13,     1,  4,     0,  0, LZ5HC_lowest_price     },  // level 6
+    { 22, 22, 17, 13,     1,  4,     0,  0, LZ5HC_lowest_price     },  // level 7
+    { 22, 22, 19, 16,     1,  4,     0,  0, LZ5HC_lowest_price     },  // level 8
+    { 22, 22, 23, 16,     3,  4,     0,  0, LZ5HC_lowest_price     },  // level 9
+    { 22, 22, 23, 16,     8,  4,     0,  0, LZ5HC_lowest_price     },  // level 10
+    { 22, 22, 23, 16,     8,  4,    12,  0, LZ5HC_optimal_price    }, // level 11
+    { 22, 22, 23, 16,     8,  4,    64,  0, LZ5HC_optimal_price    }, // level 12
+    { 22, 23, 23, 16,     8,  4,    64,  1, LZ5HC_optimal_price_bt }, // level 13
+    { 22, 23, 23, 16,   128,  4,    64,  1, LZ5HC_optimal_price_bt }, // level 14
+    { 22, 23, 28, 24, 1<<10,  4, 1<<10,  1, LZ5HC_optimal_price_bt }, // level 15
 //  { 10, 10, 10,  0,     0,  4,     0,  0, LZ5HC_fast          }, // min values
 //  { 24, 24, 28, 24, 1<<24,  7, 1<<24,  1, LZ5HC_optimal_price }, // max values
 };
--- lib/lz5hc.c
+++ lib/lz5hc.c
@@ -99,6 +99,25 @@ static void LZ5HC_init (LZ5HC_Data_Structure* ctx, const BYTE* start)
 
 
 /* Update chains up to ip (excluded) */
+FORCE_INLINE void LZ5HC_Insert3 (LZ5HC_Data_Structure* ctx, const BYTE* ip)
+{
+#if MINMATCH == 3
+    U32* HashTable3  = ctx->hashTable3;
+    const BYTE* const base = ctx->base;
+    const U32 target = (U32)(ip - base);
+    U32 idx = ctx->nextToUpdate;
+
+    while(idx < target)
+    {
+        HashTable3[LZ5HC_hash3Ptr(base+idx, ctx->params.hashLog3)] = idx;
+        idx++;
+    }
+
+    ctx->nextToUpdate = target;
+#endif 
+}
+
+/* Update chains up to ip (excluded) */
 FORCE_INLINE void LZ5HC_Insert (LZ5HC_Data_Structure* ctx, const BYTE* ip)
 {
     U32* chainTable = ctx->chainTable;
@@ -125,83 +144,6 @@ FORCE_INLINE void LZ5HC_Insert (LZ5HC_Data_Structure* ctx, const BYTE* ip)
 
     ctx->nextToUpdate = target;
 }
-
-/* Update chains up to ip (excluded) */
-FORCE_INLINE void LZ5HC_BinTree_Insert (LZ5HC_Data_Structure* ctx, const BYTE* ip, const BYTE* const iLimit)
-{
-    return;
-    
-    U32* chainTable = ctx->chainTable;
-    U32* HashTable  = ctx->hashTable;
-#if MINMATCH == 3
-    U32* HashTable3  = ctx->hashTable3;
-#endif 
-    const BYTE* const base = ctx->base;
-    const U32 dictLimit = ctx->dictLimit;
-    const U32 target = (U32)(ip - base);
-    const U32 contentMask = (1 << ctx->params.contentLog) - 1;
-    U32 idx = ctx->nextToUpdate;
-    int nbAttempts;
-    U32 *ptr0, *ptr1;
-    U32 mlen, matchIndex, delta0, delta1;
-        
-    while(idx < target)
-    {
-        U32 h = LZ5HC_hashPtr(base+idx, ctx->params.hashLog, ctx->params.searchLength);
-        matchIndex = HashTable[h];
-     //   printf("1matchIndex=%d\n", matchIndex);
-        chainTable[idx & contentMask] = (U32)(idx - matchIndex);
-        HashTable[h] = idx;
-#if MINMATCH == 3
-        HashTable3[LZ5HC_hash3Ptr(base+idx, ctx->params.hashLog3)] = idx;
-#endif 
-
-        // check rest of matches
-        ptr0 = &chainTable[(idx*2+1) & contentMask];
-        ptr1 = &chainTable[(idx*2) & contentMask];
-        delta0 = delta1 = idx - matchIndex;
-        nbAttempts = ctx->params.searchNum;
-
-        while ((matchIndex >= dictLimit) && (matchIndex < idx) && (idx - matchIndex) < MAX_DISTANCE && nbAttempts)
-        {
-            nbAttempts--;
-        //    printf("2matchIndex=%d idx=%d\n", matchIndex, idx);
-            mlen = MEM_count(base+idx, base+matchIndex, iLimit);
-            if (mlen > LZ5_OPT_NUM) break;
-
-            if (*(base+idx+mlen) < *(base+matchIndex+mlen))
-            {
-                *ptr0 = delta0;
-                ptr0 = &chainTable[(matchIndex*2) & contentMask];
-        //		printf("delta0=%d\n", delta0);
-                if (*ptr0 == (U32)-1) break;
-                delta0 = *ptr0;
-                delta1 += delta0;
-                matchIndex -= delta0;
-            }
-            else
-            {
-                *ptr1 = delta1;
-                ptr1 = &chainTable[(matchIndex*2+1) & contentMask];
-        //		printf("delta1=%d\n", delta1);
-                if (*ptr1 == (U32)-1) break;
-                delta1 = *ptr1;
-                delta0 += delta1;
-                matchIndex -= delta1;
-            }
-        }
-
-        *ptr0 = (U32)-1;
-        *ptr1 = (U32)-1;
-
-    //    LZ5_LOG_MATCH("%d: LZMAX_UPDATE_HASH_BINTREE hash=%d inp=%d,%d,%d,%d (%c%c%c%c)\n", (int)(inp-base), hash, inp[0], inp[1], inp[2], inp[3], inp[0], inp[1], inp[2], inp[3]);
-
-        idx++;
-    }
-
-    ctx->nextToUpdate = target;
-}
-
 
     
 FORCE_INLINE int LZ5HC_FindBestMatch (LZ5HC_Data_Structure* ctx,   /* Index table will be updated */
@@ -687,7 +629,6 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
     U32* const HashTable = ctx->hashTable;
     const BYTE* const base = ctx->base;
     const U32 dictLimit = ctx->dictLimit;
-    const BYTE* const lowPrefixPtr = base + dictLimit;
     const U32 maxDistance = (1 << ctx->params.windowLog);
     const U32 lowLimit = (ctx->lowLimit + maxDistance > (U32)(ip-base)) ? ctx->lowLimit : (U32)(ip - base) - (maxDistance - 1);
     const U32 contentMask = (1 << ctx->params.contentLog) - 1;
@@ -702,12 +643,11 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
     if (ip + MINMATCH > iHighLimit) return 0;
 
     /* First Match */
-//    matchIndex = HashTable[LZ5HC_hashPtr(ip, ctx->params.hashLog, ctx->params.searchLength)];
+    idx = ip - base;
     U32 h = LZ5HC_hashPtr(ip, ctx->params.hashLog, ctx->params.searchLength);
     matchIndex = HashTable[h];
-    idx = ip - base;
-    chainTable[idx & contentMask] = (U32)(idx - matchIndex);
     HashTable[h] = idx;
+    
 #if MINMATCH == 3
     h = LZ5HC_hash3Ptr(ip, ctx->params.hashLog3);
     size_t offset = idx - ctx->hashTable3[h];
@@ -720,26 +660,18 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
         {
             mlt = MEM_count(ip+MINMATCH, match+MINMATCH, iHighLimit) + MINMATCH;
 
-            int back = 0;
-#if 0
-            while ((ip+back>iLowLimit) && (match+back > lowPrefixPtr) && (ip[back-1] == match[back-1])) back--;
-            mlt -= back;
-#endif
-
             matches[mnum].off = offset;
             matches[mnum].len = mlt;
-            matches[mnum].back = -back;
+            matches[mnum].back = 0;
             mnum++;
         }
     }
 #endif
 
-
     // check rest of matches
     ptr0 = &chainTable[(idx*2+1) & contentMask];
     ptr1 = &chainTable[(idx*2) & contentMask];
     delta0 = delta1 = idx - matchIndex;
-    best_mlen = 0;
 
     while (matchIndex < idx && (matchIndex>=lowLimit) && (nbAttempts))
     {
@@ -749,25 +681,16 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
         {
             match = base + matchIndex;
 
-            if (match < ip /*&& *(ip + best_mlen) == *(match + best_mlen)*/ && (MEM_read24(match) == MEM_read24(ip)))
+            if (match < ip && (MEM_read24(match) == MEM_read24(ip)))
             {
                 mlt = MINMATCH + MEM_count(ip+MINMATCH, match+MINMATCH, iHighLimit);
-                int back = 0;
-
-#if 0
-                while ((ip+back>iLowLimit)
-                       && (match+back > lowPrefixPtr)
-                       && (ip[back-1] == match[back-1]))
-                        back--;
-                mlt -= back;
-#endif
 
                 if (mlt > best_mlen)
                 {
                     best_mlen = mlt;
                     matches[mnum].off = ip - match;
                     matches[mnum].len = mlt;
-                    matches[mnum].back = -back;
+                    matches[mnum].back = 0;
                     mnum++;
                 }
 
@@ -779,21 +702,18 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
             match = dictBase + matchIndex;
             if (MEM_read32(match) == MEM_read32(ip))
             {
-                int back=0;
                 const BYTE* vLimit = ip + (dictLimit - matchIndex);
                 if (vLimit > iHighLimit) vLimit = iHighLimit;
                 mlt = MEM_count(ip+MINMATCH, match+MINMATCH, vLimit) + MINMATCH;
                 if ((ip+mlt == vLimit) && (vLimit < iHighLimit))
                     mlt += MEM_count(ip+mlt, base+dictLimit, iHighLimit);
-                while ((ip+back > iLowLimit) && (matchIndex+back > lowLimit) && (ip[back-1] == match[back-1])) back--;
-                mlt -= back;
                 
                 if (mlt > best_mlen)
                 {
                     best_mlen = mlt;
                     matches[mnum].off = ip - match;
                     matches[mnum].len = mlt;
-                    matches[mnum].back = -back;
+                    matches[mnum].back = 0;
                     mnum++;
                 }
 
@@ -821,8 +741,6 @@ FORCE_INLINE int LZ5HC_BinTree_GetAllMatches (
             delta0 += delta1;
             matchIndex -= delta1;
         }
-
-//        matchIndex -= chainTable[matchIndex & contentMask];
     }
 
     *ptr0 = (U32)-1;
@@ -1008,7 +926,7 @@ static int LZ5HC_compress_optimal_price (
             }
             else
             {
-                LZ5HC_BinTree_Insert(ctx, ip, matchlimit);
+                LZ5HC_Insert3(ctx, ip);
                 match_num = LZ5HC_BinTree_GetAllMatches(ctx, ip, ip, matchlimit, best_mlen, matches);
             }
        }
@@ -1176,7 +1094,7 @@ static int LZ5HC_compress_optimal_price (
             }
             else
             {
-                LZ5HC_BinTree_Insert(ctx, inr, matchlimit);
+                LZ5HC_Insert3(ctx, ip);
                 match_num = LZ5HC_BinTree_GetAllMatches(ctx, inr, ip, matchlimit, best_mlen, matches);
                 LZ5_LOG_PARSER("%d: LZ5HC_BinTree_GetAllMatches match_num=%d\n", (int)((char*)inr-source), match_num);
             }
