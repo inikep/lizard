d2aacf3106cb9830296369d26c1675e49f75a194
inikep <inikep@gmail.com>
2016-08-15 11:52:56 +0200

    fixed SET_BINARY_MODE on MinGW32

--- programs/lz5io.c
+++ programs/lz5io.c
@@ -62,18 +62,18 @@
 *  OS-specific Includes
 ******************************/
 #if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32)
-#  include <fcntl.h>   /* _O_BINARY */
-#  include <io.h>      /* _setmode, _fileno, _get_osfhandle */
-#  if !defined(__DJGPP__)
-#    define SET_BINARY_MODE(file) { int unused=_setmode(_fileno(file), _O_BINARY); (void)unused; }
+#  if defined(_POSIX_C_SOURCE) || defined(_POSIX_SOURCE)
+#    include <fcntl.h>   /* _O_BINARY */
+#    include <io.h>      /* _setmode, _fileno, _get_osfhandle */
 #    include <windows.h> /* DeviceIoControl, HANDLE, FSCTL_SET_SPARSE */
+#    define SET_BINARY_MODE(file) { int unused=_setmode(_fileno(file), _O_BINARY); (void)unused; }
 #    define SET_SPARSE_FILE_MODE(file) { DWORD dw; DeviceIoControl((HANDLE) _get_osfhandle(_fileno(file)), FSCTL_SET_SPARSE, 0, 0, 0, 0, &dw, 0); }
-#    if defined(_MSC_VER) && (_MSC_VER >= 1400)  /* Avoid MSVC fseek()'s 2GiB barrier */
-#      define fseek _fseeki64
-#    endif
 #  else
-#    define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#    define SET_BINARY_MODE(file)
 #    define SET_SPARSE_FILE_MODE(file)
+#  endif
+#  if defined(_MSC_VER) && (_MSC_VER >= 1400)  /* Avoid MSVC fseek()'s 2GiB barrier */
+#    define fseek _fseeki64
 #  endif
 #else
 #  define SET_BINARY_MODE(file)
